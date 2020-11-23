482a925cc3e1e55e1588b9770b984c7177ecc4cc
Alina <nyrete@gmail.com>
2020-11-23 19:55:11 +0500

    Cлияние изменений

--- src/test/java/shouldCalculateForNotRegistered.java
+++ src/test/java/shouldCalculateForNotRegistered.java
@@ -1,4 +1,15 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class shouldCalculateForNotRegistered {
-  
+
+class shouldCalculate {
+ @Test
+ void calculate() {
+     BonusService service = new BonusService();
+     long amount = 1000_60;
+     boolean registered = false;
+     long expected = 10;
+     long actual = service.calculate(amount, registered);
+     assertEquals(expected, actual);
+ }
 }
