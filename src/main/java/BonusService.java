0c626769f287c4dc45cc45ccb9c6bd1a4126d767
Alina <nyrete@gmail.com>
2020-11-24 01:36:27 +0500

    Удаление лишних строчек

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -4,11 +4,8 @@ public class BonusService {
         long bonus = amount * percent / 100 / 100;
         long limit = 500;
         if (bonus > limit) {
-<<<<<<< HEAD
             return bonus = limit;
-=======
             bonus = limit;
->>>>>>> 4d6a983d45a3d29b9d4c84c441b0a6d04c53ade5
         }
         return bonus;
     }
